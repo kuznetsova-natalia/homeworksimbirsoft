2e81eef5af09403027a81de1dbf0b24fa5c6d7e9
kuznetsova-natalia <38231184+kuznetsova-natalia@users.noreply.github.com>
2018-04-24 20:19:24 +0300

    Initialized new repo

---
+++ app/src/main/java/com/example/nataliakuznecova/homeworksimbirsoft/AuthorizathionActivity.java
@@ -0,0 +1,13 @@
+package com.example.nataliakuznecova.homeworksimbirsoft;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AuthorizathionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authorizathion);
+    }
+}
